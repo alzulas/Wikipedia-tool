28c02ecd2feee4e26d4580e1e426200071b706d7
AdaZebra <rumusicmajor@yahoo.com>
2021-05-07 11:44:42 -0700

    test table

--- app.py
+++ app.py
@@ -39,10 +39,97 @@ def get_links():
     #print(allLinks)
     return(allLinks)
 
+def grab_all_history(url, topic, df_all_history):
+    print(url)
+    dif_url = None
+    link_still_there = True
+    page_no = 1
+    yesterday = datetime.now() - timedelta(days = 2)
+    latest_date = datetime.now()
+    # While statement loops through all pages of the change history
+    while latest_date > yesterday:
+        print("while start")
+        now = datetime.now()
+        print("Starting page " + str(page_no) + " at " + now.strftime("%m/%d/%Y, %H:%M:%S"))
+        html = urlopen(url)
+        bs = BeautifulSoup(html, 'html.parser')
+        # Changes are in <li>, but not all li
+        for li_tag in bs.find_all('li'):
+            tags = []
+            has_no_comment = True
+            first_tag_comment = True
+            line_for_append = [url, topic]
+            # data-mw-revid begins the insert for each change
+            if 'data-mw-revid' in li_tag.attrs:
+                # dates and user names exist inside of <a> tags
+                for a_tag in li_tag.find_all('a'):
+                    if str(a_tag).__contains__('prev') and str(a_tag).__contains__('diff'):
+                        dif_url = 'https://en.wikipedia.org' + str(a_tag.attrs['href'])
+                        final_marker = ['','']
+                    # Date
+                    if str(a_tag).__contains__('mw-changeslist-date'):
+                        line_for_append.append(a_tag.text)
+                        latest_date = datetime.strptime(a_tag.text, '%H:%M, %d %B %Y')
+                        if latest_date < yesterday:
+                            break
+                    # User
+                    if str(a_tag).__contains__('mw-userlink'):
+                        line_for_append.append(a_tag.text)
+                # All other data exists inside of <span> tags
+                for span_tag in li_tag.find_all('span'):
+                    # Bytes
+                    if str(span_tag).__contains__('data-mw-bytes'):
+                        line_for_append.append(span_tag.text)
+                    # Number of changes
+                    if str(span_tag).__contains__('mw-plusminus'):
+                        line_for_append.append(span_tag.text)
+                    # Comments
+                    if str(span_tag).__contains__('comment') and has_no_comment:
+                        line_for_append.append(span_tag.text)
+                        has_no_comment = False
+                    # Tags
+                    if str(span_tag).__contains__('mw-tag-marker'):
+                        if first_tag_comment:
+                            first_tag_comment = False
+                        else:
+                            splits = re.split('>', str(span_tag))
+                            if str(span_tag).__contains__('href'):
+                                tags.append(splits[-3][:-3])
+                            else:
+                                tags.append(splits[-2][:-6])
+                # In the case that there are no comments
+                if has_no_comment:
+                    line_for_append.append("No comment")
+            line_for_append.append(tags)
+            if dif_url:
+                line_for_append.append(dif_url)
+            # add line to dataframe
+            try:
+                df_all_history.loc[len(df_all_history)] = line_for_append
+            except:
+                continue
+        url = ''
+        # Get next page of history
+        for a_tag in bs.find_all('a'):
+            if str(a_tag).__contains__('mw-nextlink'):
+                url = 'https://en.wikipedia.org' + str(a_tag.attrs['href'])
+        # If no next page, break while loop
+        link_still_there = False
+    return(df_all_history)
+
 @app.route('/')
 def index():
     links = get_links()
-    return render_template('index.html', links=links)
+    df_all_history = pd.DataFrame(columns=['Page', 'Topic', 'Date of Change', 'User', 'Bytes', 'Number of Changes', 'Comments', 'Tags', 'Link To Diff'])
+    for topic in tqdm(allLinks):
+        url = 'https://en.wikipedia.org/w/index.php?title=:' + topic[6:] + '&action=history'
+        topic = topic[6:]
+        df_all_history = pd.concat([df_all_history, grab_all_history(url, topic, df_all_history)])
+        shape_of_data = df_all_history.shape
+        if shape_of_data[0] > 20:
+            history_list = [df_all_history.columns.values.tolist()] + df_all_history.values.tolist()
+            break
+    return render_template('index.html', table_list=history_list)
 
 @app.route('/about')
 def about():
